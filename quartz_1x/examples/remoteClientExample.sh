e87a044ff718c53622383b940fd53d8c7b26af8d
wkratzer <wkratzer@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2005-03-20 20:42:58 +0000

    Issue number: Obtained from: Submitted by:   Bill Kratzer (wkratzer@dev.java.net) Reviewed by:
    Removing old example files.

--- quartz_1x/examples/remoteClientExample.sh
+++
@@ -1,6 +0,0 @@
-
-
-QRTZ_CP=../lib/commons-logging.jar:../lib/commons-collections.jar:../lib/commons-dbcp-1.1.jar:../lib/commons-pool-1.1.jar:../lib/log4j.jar:../lib/jdbc2_0-stdext.jar:../lib/quartz.jar:../lib/examples.jar
-
-
-java -cp $QRTZ_CP -Dorg.quartz.properties=remoteClient.properties org.quartz.examples.QuartzRemoteClient
