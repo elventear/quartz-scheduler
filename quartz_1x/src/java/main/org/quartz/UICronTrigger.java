e2cc6baae9310d0653c2e2250d7c4cfed0629d67
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2005-05-24 18:12:24 +0000

    Updated copyright and set license to Apache 2.0

--- quartz_1x/src/java/main/org/quartz/UICronTrigger.java
+++ quartz_1x/src/java/main/org/quartz/UICronTrigger.java
@@ -1,7 +1,22 @@
-/*
- * Copyright (c) 2004-2005 by OpenSymphony
- * All rights reserved.
+
+/* 
+ * Copyright 2004-2005 OpenSymphony 
  * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations 
+ * under the License.
+ * 
+ */
+
+/*
  * Previously Copyright (c) 2001-2004 James House
  */
 package org.quartz;
@@ -128,6 +143,7 @@ import java.text.ParseException;
  * 
  * @author Brian Topping
  * @author based on code by Sharada Jambula, James House, Mads Henderson
+ * @deprecated
  */
 public class UICronTrigger extends Trigger {
 
