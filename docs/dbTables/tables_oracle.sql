a779a8b6e3c696c6e249ebac4fee5be8cd95f312
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2005-09-17 19:44:00 +0000

    Issue number:  QUARTZ-224 Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- docs/dbTables/tables_oracle.sql
+++ docs/dbTables/tables_oracle.sql
@@ -1,3 +1,9 @@
+--
+-- A hint submitted by a user: Oracle DB MUST be created as "shared" and the 
+-- job_queue_processes parameter  must be greater than 2, otherwise a DB lock 
+-- will happen.   However, these settings are pretty much standard after any
+-- Oracle install, so most users need not worry about this.
+--
 
 delete from qrtz_job_listeners;
 delete from qrtz_trigger_listeners;
