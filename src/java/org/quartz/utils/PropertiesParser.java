fe268cbc327c4c581c358f98ac300d8e5a140d90
jrosenberg <jrosenberg@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-05-10 21:48:07 +0000

    Issue number: QUARTZ-441 Enhance PropertiesParser.getPropertyGroup() to also support an array of prefixes to ignore, so that the lockHandler properties can be excluded when setting the "org.quartz.jobStore" bean properties.

--- src/java/org/quartz/utils/PropertiesParser.java
+++ src/java/org/quartz/utils/PropertiesParser.java
@@ -351,10 +351,29 @@ public class PropertiesParser {
     }
 
     public Properties getPropertyGroup(String prefix) {
-        return getPropertyGroup(prefix, false);
+        return getPropertyGroup(prefix, false, null);
     }
 
     public Properties getPropertyGroup(String prefix, boolean stripPrefix) {
+        return getPropertyGroup(prefix, stripPrefix, null);
+    }
+
+    /**
+     * Get all properties that start with the given prefix.  
+     * 
+     * @param prefix The prefix for which to search.  If it does not end in 
+     *      a "." then one will be added to it for search purposes.
+     * @param stripPrefix Whether to strip off the given <code>prefix</code>
+     *      in the result's keys.
+     * @param excludedPrefixes Optional array of fully qualified prefixes to
+     *      exclude.  For example if <code>prefix</code> is "a.b.c", then 
+     *      <code>excludedPrefixes</code> might be "a.b.c.ignore".
+     *      
+     * @return Group of <code>Properties</code> that start with the given prefix, 
+     *      optionally have that prefix removed, and do not include properties 
+     *      that start with one of the given excluded prefixes.
+     */
+    public Properties getPropertyGroup(String prefix, boolean stripPrefix, String[] excludedPrefixes) {
         Enumeration keys = props.propertyNames();
         Properties group = new Properties();
 
@@ -365,12 +384,22 @@ public class PropertiesParser {
         while (keys.hasMoreElements()) {
             String key = (String) keys.nextElement();
             if (key.startsWith(prefix)) {
-                String value = getStringProperty(key);
                 
-                if (stripPrefix) { 
-                    group.put(key.substring(prefix.length()), value);
-                } else {
-                    group.put(key, value);
+                boolean exclude = false;
+                if (excludedPrefixes != null) {
+                    for (int i = 0; (i < excludedPrefixes.length) && (exclude == false); i++) {
+                        exclude = key.startsWith(excludedPrefixes[i]);
+                    }
+                }
+
+                if (exclude == false) {
+                    String value = getStringProperty(key);
+                    
+                    if (stripPrefix) { 
+                        group.put(key.substring(prefix.length()), value);
+                    } else {
+                        group.put(key, value);
+                    }
                 }
             }
         }
