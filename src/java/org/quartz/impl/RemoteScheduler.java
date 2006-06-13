0a0c98fe57439058e2d869dcb1a3653411e9b5d9
jrosenberg <jrosenberg@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-06-13 12:50:45 +0000

    Issue number: QUARTZ-467 When shutdown(), remove the RemoteScheduler instance from the SchedulerRepository

--- src/java/org/quartz/impl/RemoteScheduler.java
+++ src/java/org/quartz/impl/RemoteScheduler.java
@@ -289,7 +289,11 @@ public class RemoteScheduler implements Scheduler {
      */
     public void shutdown() throws SchedulerException {
         try {
+            String schedulerName = getSchedulerName();
+            
             getRemoteScheduler().shutdown();
+            
+            SchedulerRepository.getInstance().remove(schedulerName);
         } catch (RemoteException re) {
             throw invalidateHandleCreateException(
                     "Error communicating with remote scheduler.", re);
@@ -304,7 +308,11 @@ public class RemoteScheduler implements Scheduler {
     public void shutdown(boolean waitForJobsToComplete)
         throws SchedulerException {
         try {
+            String schedulerName = getSchedulerName();
+            
             getRemoteScheduler().shutdown(waitForJobsToComplete);
+
+            SchedulerRepository.getInstance().remove(schedulerName);
         } catch (RemoteException re) {
             throw invalidateHandleCreateException(
                     "Error communicating with remote scheduler.", re);
