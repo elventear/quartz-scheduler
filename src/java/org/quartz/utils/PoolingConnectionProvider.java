952883dbab2d90d65b1af8f38ca2387fb7b81406
jrosenberg <jrosenberg@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-07-05 15:20:23 +0000

    Issue number: QUARTZ-480 Improved commenting, made validation consistant across constructors, and made user/password empty string rather than null when no property found to be consistent with StdSchedulerFactory.

--- src/java/org/quartz/utils/PoolingConnectionProvider.java
+++ src/java/org/quartz/utils/PoolingConnectionProvider.java
@@ -28,13 +28,13 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 /**
  * <p>
- * A <code>ConnectionProvider</code> implementation that creates it's own
+ * A <code>ConnectionProvider</code> implementation that creates its own
  * pool of connections.
  * </p>
  * 
  * <p>
- * This class uses <a href="http://jakarta.apache.org/commons/index.html">DBCP
- * </a>, an Apache-Jakarta-Commons product.
+ * This class uses <a href="http://jakarta.apache.org/commons/dbcp/">DBCP</a>, 
+ * an Apache-Jakarta-Commons product.
  * </p>
  * 
  * @see DBConnectionManager
@@ -54,25 +54,29 @@ public class PoolingConnectionProvider implements ConnectionProvider {
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
-    public static final String DB_PROPS_PREFIX = "org.quartz.db.";
-
-    public static final String DB_JNDI_DATASOURCE_URL = "jndiURL";
-
-    // The JDBC database driver
+    /** The JDBC database driver. */
     public static final String DB_DRIVER = "driver";
 
-    // The JDBC database URL
+    /** The JDBC database URL. */
     public static final String DB_URL = "URL";
 
-    // The database user name
+    /** The database user name. */
     public static final String DB_USER = "user";
 
-    // The database user password
+    /** The database user password. */
     public static final String DB_PASSWORD = "password";
 
+    /** The maximum number of database connections to have in the pool. */
     public static final String DB_MAX_CONNECTIONS = "maxConnections";
 
+    /** 
+     * The database sql query to execute everytime a connection is retrieved 
+     * from the pool to ensure that it is still valid. 
+     */
     public static final String DB_VALIDATION_QUERY = "validationQuery";
+    
+    /** Default maximum number of database connections in the pool. */
+    public static final int DEFAULT_DB_MAX_CONNECTIONS = 10; 
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -95,12 +99,41 @@ public class PoolingConnectionProvider implements ConnectionProvider {
     public PoolingConnectionProvider(String dbDriver, String dbURL,
             String dbUser, String dbPassword, int maxConnections,
             String dbValidationQuery) throws SQLException {
-
-        initialize(dbDriver, dbURL, dbUser, dbPassword, maxConnections,
-                dbValidationQuery);
-
+        initialize(
+            dbDriver, dbURL, dbUser, dbPassword, 
+            maxConnections, dbValidationQuery);
     }
 
+    /**
+     * Create a connection pool using the given properties.
+     * 
+     * <p>
+     * The properties passed should contain:
+     * <UL>
+     * <LI>{@link #DB_DRIVER}- The database driver class name
+     * <LI>{@link #DB_URL}- The database URL
+     * <LI>{@link #DB_USER}- The database user
+     * <LI>{@link #DB_PASSWORD}- The database password
+     * <LI>{@link #DB_MAX_CONNECTIONS}- The maximum # connections in the pool,
+     * optional
+     * <LI>{@link #DB_VALIDATION_QUERY}- The sql validation query, optional
+     * </UL>
+     * </p>
+     * 
+     * @param config
+     *            configuration properties
+     */
+    public PoolingConnectionProvider(Properties config) throws SQLException {
+        PropertiesParser cfg = new PropertiesParser(config);
+        initialize(
+            cfg.getStringProperty(DB_DRIVER), 
+            cfg.getStringProperty(DB_URL), 
+            cfg.getStringProperty(DB_USER, ""), 
+            cfg.getStringProperty(DB_PASSWORD, ""), 
+            cfg.getIntProperty(DB_MAX_CONNECTIONS, DEFAULT_DB_MAX_CONNECTIONS), 
+            cfg.getStringProperty(DB_VALIDATION_QUERY));
+    }
+    
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      * 
@@ -108,19 +141,33 @@ public class PoolingConnectionProvider implements ConnectionProvider {
      * 
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
-
-    private void initialize(String dbDriver, String dbURL, String dbUser,
-            String dbPassword, int maxConnections, String dbValidationQuery)
-        throws SQLException {
-        if (dbDriver == null) {
-            throw new SQLException("DB driver class name cannot be null!");
-        }
+    
+    /**
+     * Create the underlying DBCP BasicDataSource with the 
+     * default supported properties.
+     */
+    private void initialize(
+        String dbDriver, 
+        String dbURL, 
+        String dbUser,
+        String dbPassword, 
+        int maxConnections, 
+        String dbValidationQuery) throws SQLException {
         if (dbURL == null) {
-            throw new SQLException("DB URL cannot be null!");
+            throw new SQLException(
+                "DBPool could not be created: DB URL cannot be null");
         }
+        
+        if (dbDriver == null) {
+            throw new SQLException(
+                "DBPool '" + dbURL + "' could not be created: " +
+                "DB driver class name cannot be null!");
+        }
+        
         if (maxConnections < 0) {
             throw new SQLException(
-                    "Max connections must be greater than zero!");
+                "DBPool '" + dbURL + "' could not be created: " + 
+                "Max connections must be greater than zero!");
         }
 
         datasource = new BasicDataSource();
@@ -133,51 +180,24 @@ public class PoolingConnectionProvider implements ConnectionProvider {
             datasource.setValidationQuery(dbValidationQuery);
         }
     }
-
+    
     /**
+     * Get the DBCP BasicDataSource created during initialization.
+     * 
      * <p>
-     * Create a connection pool using the given properties.
+     * This can be used to set additional data source properties in a 
+     * subclass's constructor.
      * </p>
-     * 
-     * <p>
-     * The properties passed should contain either
-     * <UL>
-     * <LI>JNDI DataSource URL {@link #DB_JNDI_DATASOURCE_URL}
-     * </UL>
-     * or
-     * <UL>
-     * <LI>{@link #DB_DRIVER}- The database driver class name
-     * <LI>{@link #DB_URL}- The database URL
-     * <LI>{@link #DB_USER}- The database user
-     * <LI>{@link #DB_PASSWORD}- The database password
-     * <LI>{@link #DB_MAX_CONNECTIONS}- The maximum # connections in the pool
-     * </UL>
-     * <P>
-     * 
-     * @param config
-     *          configuration properties
-     * @exception SQLException
-     *              if an error occurs
      */
-    public PoolingConnectionProvider(Properties config) throws SQLException {
-        PropertiesParser cfg = new PropertiesParser(config);
-        String url = config.getProperty(DB_URL);
-        try {
-            initialize(config.getProperty(DB_DRIVER), url, config
-                    .getProperty(DB_USER), config.getProperty(DB_PASSWORD), cfg
-                    .getIntProperty(DB_MAX_CONNECTIONS, 3), cfg
-                    .getStringProperty(DB_VALIDATION_QUERY));
-        } catch (Exception e) {
-            throw new SQLException("DBPool '" + url
-                    + "' could not be created: " + e.toString());
-        }
+    protected BasicDataSource getDataSource() {
+        return datasource;
     }
 
     public Connection getConnection() throws SQLException {
-        return this.datasource.getConnection();
+        return datasource.getConnection();
     }
     
     public void shutdown() throws SQLException {
-        this.datasource.close();
+        datasource.close();
     }    
 }
