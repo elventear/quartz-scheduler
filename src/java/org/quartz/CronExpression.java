e25ebbc0d7d2d0fe46e099344e99a7f7aabc9f3a
eagle79 <eagle79@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-07-06 03:25:06 +0000

    Issue number: QUARTZ-445 corrected bug in getNextIncludedTime() which causes it to loop forever in some cases.

--- src/java/org/quartz/CronExpression.java
+++ src/java/org/quartz/CronExpression.java
@@ -281,6 +281,43 @@ public class CronExpression implements Serializable, Cloneable {
     }
     
     /**
+     * Returns the next date/time <I>after</I> the given date/time which does
+     * <I>not</I> satisfy the expression
+     * 
+     * @param date the date/time at which to begin the search for the next 
+     *             invalid date/time
+     * @return the next valid date/time
+     */
+    public Date getNextInvalidTimeAfter(Date date) {
+    	long difference = 1000;
+    	
+    	//move back to the nearest second so differences will be accurate
+    	Calendar adjustCal = Calendar.getInstance();
+    	adjustCal.setTime(date);
+    	adjustCal.set(Calendar.MILLISECOND, 0);
+    	Date lastDate = adjustCal.getTime();
+    	
+    	Date newDate = null;
+    	
+    	//TODO: (QUARTZ-481) IMPROVE THIS! The following is a BAD solution to this problem. Performance will be very bad here, depending on the cron expression. It is, however A solution.
+    	
+    	//keep getting the next included time until it's farther than one second
+    	// apart. At that point, lastDate is the last valid fire time. We return
+    	// the second immediately following it.
+    	while (difference == 1000) {
+    		newDate = getTimeAfter(lastDate);
+    		
+    		difference = newDate.getTime() - lastDate.getTime();
+    		
+    		if (difference == 1000) {
+    			lastDate = newDate;
+    		} 
+    	}
+    	
+    	return new Date(lastDate.getTime() + 1000);
+    }
+    
+    /**
      * Returns the time zone for which this <code>CronExpression</code> 
      * will be resolved.
      */
