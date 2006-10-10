eedada335f37f5f2c98ca2b35525e6aceb14ff5e
perfnorm <perfnorm@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-10-10 01:22:54 +0000

    Issue number: QUARTZ-519 In getPropertyGroup(), the default value for an empty property should be empty string so as to not cause a null pointer exception when trying to insert into a Properties obj (Hashtable)

--- src/java/org/quartz/utils/PropertiesParser.java
+++ src/java/org/quartz/utils/PropertiesParser.java
@@ -393,7 +393,7 @@ public class PropertiesParser {
                 }
 
                 if (exclude == false) {
-                    String value = getStringProperty(key);
+                    String value = getStringProperty(key, "");
                     
                     if (stripPrefix) { 
                         group.put(key.substring(prefix.length()), value);
