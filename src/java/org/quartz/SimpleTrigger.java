c04e059e5626d25a13522023a231c8ca8713739c
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-10-23 21:44:26 +0000

    fix possible divide by zero.

--- src/java/org/quartz/SimpleTrigger.java
+++ src/java/org/quartz/SimpleTrigger.java
@@ -792,6 +792,11 @@ public class SimpleTrigger extends Trigger {
     }
 
     public int computeNumTimesFiredBetween(Date start, Date end) {
+
+        if(repeatInterval < 1) {
+            return 0;
+        }
+        
         long time = end.getTime() - start.getTime();
 
         return (int) (time / repeatInterval);
