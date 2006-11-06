49369427baaf4c98ca8bde3aa92f22e42b88426a
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2006-11-06 17:08:23 +0000

    QUARTZ-421 - Trigger with empty JobDataMap can't be loaded with some dbs (such as MySQL)

--- src/java/org/quartz/impl/jdbcjobstore/StdJDBCDelegate.java
+++ src/java/org/quartz/impl/jdbcjobstore/StdJDBCDelegate.java
@@ -3463,14 +3463,20 @@ public class StdJDBCDelegate implements DriverDelegate, StdJDBCConstants {
             InputStream binaryInput = blobLocator.getBinaryStream();
 
             if (null != binaryInput) {
-                ObjectInputStream in = new ObjectInputStream(binaryInput);
-                try {
-                    obj = in.readObject();
-                } finally {
-                    in.close();
+                if (binaryInput instanceof ByteArrayInputStream
+                    && ((ByteArrayInputStream) binaryInput).available() == 0 ) {
+                    //do nothing
                 }
-
+                else {
+                    ObjectInputStream in = new ObjectInputStream(binaryInput);
+                    try {
+                        obj = in.readObject();
+                    } finally {
+                        in.close();
+                    }
+                }
             }
+
         }
         return obj;
     }
