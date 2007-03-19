fb2fac5345239c164f2e540e7c2ad741ac23456c
bayard <bayard@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2007-03-19 05:51:54 +0000

    Fixing the classname in the example javadoc as per QUARTZ-545

--- src/java/org/quartz/ee/servlet/QuartzInitializerListener.java
+++ src/java/org/quartz/ee/servlet/QuartzInitializerListener.java
@@ -51,7 +51,7 @@ import org.quartz.impl.StdSchedulerFactory;
  *     
  *     &lt;listener&gt;
  *         &lt;listener-class&gt;
- *             org.quartz.ee.servlet.QuartzInitializerServletListener
+ *             org.quartz.ee.servlet.QuartzInitializerListener
  *         &lt;/listener-class&gt;
  *     &lt;/listener&gt;
  * </pre>
