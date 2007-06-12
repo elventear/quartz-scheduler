8ec16ef137c852f8a46b87cf442bf4cc9920f5b7
bayard <bayard@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2007-06-12 19:33:58 +0000

    Fixing paths to jars - untested

--- examples/bin/buildcp.bat
+++ examples/bin/buildcp.bat
@@ -7,10 +7,10 @@ SET QEB=%QUARTZ%\examples\bin
 SET TMP_CP=.
 
 dir /b "%QUARTZ%\*.jar" > temp.tmp
-FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\%%I"
+FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\build\%%I"
 
 dir /b "%QUARTZ%\examples\*.jar" > temp.tmp
-FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\examples\%%I"
+FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\examples\build\%%I"
 
 dir /b "%QUARTZ%\lib\core\*.jar" > temp.tmp
 FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\lib\core\%%I"
