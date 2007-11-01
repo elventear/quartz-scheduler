c182e91e17a87a3a65186d479f6f1a7288c7de35
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2007-11-01 00:59:00 +0000

    QUARTZ-625 : NullPointer in org.quartz.utils.Pair.hashCode() method

--- src/java/org/quartz/utils/Pair.java
+++ src/java/org/quartz/utils/Pair.java
@@ -1,18 +1,18 @@
-/* 
- * Copyright 2004-2005 OpenSymphony 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * Copyright 2004-2005 OpenSymphony
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
- * 
+ *
  */
 
 /*
@@ -25,16 +25,16 @@ package org.quartz.utils;
  * <p>
  * Utility class for storing two pieces of information together.
  * </p>
- * 
+ *
  * @author <a href="mailto:jeff@binaryfeed.org">Jeffrey Wescott</a>
  */
 public class Pair {
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     * 
+     *
      * Data members.
-     * 
+     *
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
@@ -42,11 +42,21 @@ public class Pair {
 
     private Object second;
 
+
+    public Pair()
+    {}
+
+    public Pair(Object first, Object second)
+    {
+        setFirst(first);
+        setSecond(second);
+    }
+
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     * 
+     *
      * Interface.
-     * 
+     *
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
@@ -54,7 +64,7 @@ public class Pair {
      * <p>
      * Get the first object in the pair.
      * </p>
-     * 
+     *
      * @return the first object
      */
     public final Object getFirst() {
@@ -65,7 +75,7 @@ public class Pair {
      * <p>
      * Set the value of the first object in the pair.
      * </p>
-     * 
+     *
      * @param first
      *          the first object
      */
@@ -77,7 +87,7 @@ public class Pair {
      * <p>
      * Get the second object in the pair.
      * </p>
-     * 
+     *
      * @return the second object
      */
     public final Object getSecond() {
@@ -88,7 +98,7 @@ public class Pair {
      * <p>
      * Set the second object in the pair.
      * </p>
-     * 
+     *
      * @param second
      *          the second object
      */
@@ -100,7 +110,7 @@ public class Pair {
      * <p>
      * Test equality of this object with that.
      * </p>
-     * 
+     *
      * @param that
      *          object to compare
      * @return true if objects are equal, false otherwise
@@ -111,16 +121,30 @@ public class Pair {
         } else {
             try {
                 Pair other = (Pair) that;
-                return (this.first.equals(other.first) && this.second
+                if(first == null && second == null)
+                    return (other.first == null && other.second == null)
+                if(first != null && second != null)
+                    return (this.first.equals(other.first) && this.second
                         .equals(other.second));
+                if(first != null)
+                    return this.first.equals(other.first);
+                if(second != null)
+                    return this.second.equals(other.second);
+
             } catch (ClassCastException e) {
                 return false;
             }
         }
     }
-    
+
     public int hashCode() {
-        return (17 * first.hashCode()) + second.hashCode();
+        if(first != null && second != null)
+            return (first.hashCode() ^ second.hashCode());
+        if(first != null)
+            return (17 ^ first.hashCode());
+        if(second != null)
+            return (17 ^ second.hashCode());
+        return super.hashCode();
     }
 }
 
