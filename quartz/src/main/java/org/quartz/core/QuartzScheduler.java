9eeb15bee81a9a88cec2bc3b4bebdf7053931d72
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2010-02-01 07:55:20 +0000

    DEV-3697: update checker for plain Quartz

--- quartz/src/main/java/org/quartz/core/QuartzScheduler.java
+++ quartz/src/main/java/org/quartz/core/QuartzScheduler.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
+import java.util.Timer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -50,15 +51,16 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerContext;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerListener;
-import org.quartz.listeners.SchedulerListenerSupport;
 import org.quartz.Trigger;
 import org.quartz.TriggerListener;
 import org.quartz.UnableToInterruptJobException;
 import org.quartz.impl.SchedulerRepository;
+import org.quartz.listeners.SchedulerListenerSupport;
 import org.quartz.simpl.SimpleJobFactory;
 import org.quartz.spi.JobFactory;
 import org.quartz.spi.SchedulerPlugin;
 import org.quartz.spi.SchedulerSignaler;
+import org.quartz.utils.UpdateChecker;
 
 /**
  * <p>
@@ -213,6 +215,8 @@ public class QuartzScheduler implements RemotableQuartzScheduler {
 
         signaler = new SchedulerSignalerImpl(this, this.schedThread);
         
+        scheduleUpdateCheck();
+        
         getLog().info("Quartz Scheduler v." + getVersion() + " created.");
     }
 
@@ -248,6 +252,13 @@ public class QuartzScheduler implements RemotableQuartzScheduler {
     public Log getLog() {
         return log;
     }
+    
+    /**
+     * Update checker scheduler - fires every week
+     */
+    private void scheduleUpdateCheck() {
+        new Timer(true).scheduleAtFixedRate(new UpdateChecker(), 1, 7 * 24 * 60 * 60 * 1000L);
+    }
 
     /**
      * Register the scheduler in the local MBeanServer.
@@ -259,8 +270,7 @@ public class QuartzScheduler implements RemotableQuartzScheduler {
         String jmxObjectName = resources.getJMXObjectName();
         
         registry.registerComponent(this, jmxObjectName, null);
-        
-        getLog().info("Scheduler registered with local MBeanServer under name '" + jmxObjectName + "'");
+                getLog().info("Scheduler registered with local MBeanServer under name '" + jmxObjectName + "'");
     }
 
     /**
