d71d073a06e38f41e6b2f0f42f638ec49d3d5d55
gkeim <gkeim@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2010-04-07 22:20:59 +0000

    Protected JMX ObjectName from invalid characters in user-provided names.

--- quartz/src/main/java/org/quartz/core/QuartzSchedulerResources.java
+++ quartz/src/main/java/org/quartz/core/QuartzSchedulerResources.java
@@ -503,14 +503,14 @@ public class QuartzSchedulerResources {
     /**
      * Create the name under which this scheduler should be registered in JMX.
      * <p>
-     * The name is composed as: 
-     *  quartz:type=QuartzScheduler,name=<i>[schedName]</i>,instance=<i>[schedInstId]</i>
+     * The name is composed as:
+     * quartz:type=QuartzScheduler,name=<i>[schedName]</i>,instance=<i>[schedInstId]</i>
      * </p>
      */
     public static String generateJMXObjectName(String schedName, String schedInstId) {
-        return "quartz:type=QuartzScheduler" + 
-            ",name=" + schedName + 
-            ",instance=" + schedInstId;
+        return "quartz:type=QuartzScheduler" + ",name="
+            + schedName.replaceAll(":|=|\n", ".")
+            + ",instance=" + schedInstId;
     }
 
 }
