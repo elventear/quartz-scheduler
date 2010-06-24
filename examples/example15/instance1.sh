80c69e8922c5a04f1f14861759d0f466c019e4e8
lorban <lorban@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2010-06-24 07:19:41 +0000

    added TC job store example

---
+++ examples/example15/instance1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Change this to your JDK installation root
+#
+#JAVA_HOME=/usr/java/j2sdk1.4.0_01
+
+JRE=$JAVA_HOME/jre
+JAVA=$JRE/bin/java
+
+QUARTZ=../..
+
+. ${QUARTZ}/examples/bin/buildcp.sh
+
+#
+# Set the path to your Terracotta server home here
+TC_HOME=/home/user/tc
+
+TC_CP=$TC_HOME/quartz/quartz-terracotta-1.2.0.jar:$TC_HOME/common/terracotta-toolkit-1.0-runtime-1.0.0.jar
+
+
+QUARTZ_CP=$QUARTZ_CP:$TC_CP
+
+# Uncomment the following line if you would like to set log4j
+# logging properties
+#
+#LOGGING_PROPS="-Dlog4j.configuration=log4j.properties"
+
+# Set the name and location of the quartz.properties file
+QUARTZ_PROPS="-Dorg.quartz.properties=instance1.properties"
+
+$JAVA -classpath $QUARTZ_CP $QUARTZ_PROPS $LOGGING_PROPS org.quartz.examples.example15.ClusterExample $1
+
---
+++ examples/example15/instance2.bat
@@ -0,0 +1,30 @@
+@echo off
+
+rem Set Quartz to the base directory of the Quartz Distribution
+@SET QUARTZ=..\..
+
+@rem setup the class path...
+CALL ..\bin\buildcp.bat
+SET QUARTZ_CP=%TMP_CP%
+
+rem !!!!!!! Please read important information. !!!!!!
+rem If "java" is not in your path, please set the path 
+rem for Java 2 Runtime Environment in the path variable below
+rem for example :
+rem @SET PATH=D:\jdk1.3.1;%PATH%
+rem 
+
+
+rem Set LOG4J props if you are interested in setting up
+rem a configuraiton file for log4j logging
+rem @SET LOG4J_PROPS="-Dlog4j.configuration=log4j.properties"
+
+rem Set the location and name of the quartz.properties file
+@SET QUARTZ_PROPS="-Dorg.quartz.properties=instance2.properties"
+
+rem Set the path to your Terracotta server home here
+@SET TC_HOME=..\..\..
+
+@SET TC_CP=%TC_HOME%/quartz/quartz-terracotta-1.2.0.jar;%TC_HOME%/common/terracotta-toolkit-1.0-runtime-1.0.0.jar
+
+"java" -cp "%QUARTZ_CP%;%TC_CP%" %QUARTZ_PROPS% %LOG4J_PROPS% org.quartz.examples.example13.ClusterExample dontScheduleJobs
