cecf726fc8eadf843fa731f68dac5c3774be158f
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2010-06-29 23:44:49 +0000

    DEV-4369: merge 1221 from trunk

---
+++ examples/example15/start-sample-server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+#
+
+
+# Set the path to your Terracotta server home here
+TC_HOME=../../..
+
+exec $TC_HOME/bin/start-tc-server.sh&
