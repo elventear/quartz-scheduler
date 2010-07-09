82429519e3c3e59381f924dd50b94d5b54351409
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2010-07-09 00:43:06 +0000

    merge 1247 from quartz-1.8.x DEV-4411, QTZ-57

--- examples/bin/buildcp.bat
+++ examples/bin/buildcp.bat
@@ -1,30 +1,18 @@
 @echo off
-SET WORKDIR=%~d0%~p0
 
-IF "%QUARTZ%"=="" SET QUARTZ=%WORKDIR%..\..
+IF "%QUARTZ%"=="" SET QUARTZ=..\..
 
 SET QEB=%QUARTZ%\examples\bin
 
 SET TMP_CP=.
 
-dir /b "%QUARTZ%\*.jar" 1 > temp.tmp 2> error.tmp
+dir /b "%QUARTZ%\*.jar" > temp.tmp
 FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\%%I"
 
-dir /b "%QUARTZ%\examples\*.jar" 1 > temp.tmp 2> error.tmp
-FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\examples\%%I"
-
-dir /b "%QUARTZ%\*.jar" 1 > temp.tmp 2> error.tmp
+dir /b "%QUARTZ%\*.jar" > temp.tmp
 FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\build\%%I"
 
-dir /b "%QUARTZ%\examples\*.jar" 1 > temp.tmp 2> error.tmp
-FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\examples\build\%%I"
-
-dir /b "%QUARTZ%\lib\*.jar" 1 > temp.tmp 2> error.tmp
+dir /b "%QUARTZ%\lib\*.jar" > temp.tmp
 FOR /F %%I IN (temp.tmp) DO CALL "%QEB%\addpath.bat" "%QUARTZ%\lib\%%I"
 
-DEL error.tmp
 DEL temp.tmp
-
-IF NOT "%CLASSPATH%"=="" SET TMP_CP=%TMP_CP%;"%CLASSPATH%"
-
-
