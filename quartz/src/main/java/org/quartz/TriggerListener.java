b99a1c1b79f79a2316f4b3e342418cfa6aaca000
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-04-01 02:56:21 +0000

    merger 1496,1497

--- quartz/src/main/java/org/quartz/TriggerListener.java
+++ quartz/src/main/java/org/quartz/TriggerListener.java
@@ -75,7 +75,7 @@ public interface TriggerListener {
      * Called by the <code>{@link Scheduler}</code> when a <code>{@link Trigger}</code>
      * has fired, and it's associated <code>{@link org.quartz.JobDetail}</code>
      * is about to be executed.  If the implementation vetos the execution (via
-     * returning <code>true</code>, the job's execute method will not be called.
+     * returning <code>true</code>), the job's execute method will not be called.
      * </p>
      * 
      * <p>
