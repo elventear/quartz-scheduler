790c0acc1dba89ebd55b7948c299d97ba7c90d35
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-05-03 23:28:33 +0000

    QTZ-172: Problem validating new misfire instructions

--- quartz/src/test/java/org/quartz/CalendarIntervalTriggerTest.java
+++ quartz/src/test/java/org/quartz/CalendarIntervalTriggerTest.java
@@ -15,6 +15,7 @@
  */
 package org.quartz;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -294,6 +295,28 @@ public class CalendarIntervalTriggerTest  extends SerializationTestSupport {
         endCalendar.add(Calendar.MINUTE,-3); // back up three more minutes
         
         assertTrue("Final fire time not computed correctly for minutely interval.", (endCalendar.getTime().equals(testTime)));
+    }
+    
+    public void testMisfireInstructionValidity() throws ParseException {
+        CalendarIntervalTriggerImpl trigger = new CalendarIntervalTriggerImpl();
+
+        try {
+            trigger.setMisfireInstruction(Trigger.MISFIRE_INSTRUCTION_IGNORE_MISFIRE_POLICY);
+            trigger.setMisfireInstruction(Trigger.MISFIRE_INSTRUCTION_SMART_POLICY);
+            trigger.setMisfireInstruction(CalendarIntervalTriggerImpl.MISFIRE_INSTRUCTION_DO_NOTHING);
+            trigger.setMisfireInstruction(CalendarIntervalTriggerImpl.MISFIRE_INSTRUCTION_FIRE_ONCE_NOW);
+        }
+        catch(Exception e) {
+            fail("Unexpected exception while setting misfire instruction.");
+        }
+        
+        try {
+            trigger.setMisfireInstruction(CalendarIntervalTriggerImpl.MISFIRE_INSTRUCTION_DO_NOTHING + 1);
+            
+            fail("Expected exception while setting invalid misfire instruction but did not get it.");
+        }
+        catch(Exception e) {
+        }
     }
     
     @Override
