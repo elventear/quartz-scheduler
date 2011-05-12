f7ffbf8224e033a170cc88bc6e5466d8d624f62a
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-05-12 03:50:42 +0000

    QTZ-174 : Race condition during shutdown of Quartz within JDBCJobStore's misfire handling thread.

--- quartz/src/main/java/org/quartz/impl/jdbcjobstore/JobStoreSupport.java
+++ quartz/src/main/java/org/quartz/impl/jdbcjobstore/JobStoreSupport.java
@@ -648,12 +648,20 @@ public abstract class JobStoreSupport implements JobStore, Constants {
      * </p>
      */
     public void shutdown() {
-        if (clusterManagementThread != null) {
-            clusterManagementThread.shutdown();
-        }
-
         if (misfireHandler != null) {
             misfireHandler.shutdown();
+            try {
+                misfireHandler.join();
+            } catch (InterruptedException ignoreInterruptionOfThisThread) {
+            }
+        }
+
+        if (clusterManagementThread != null) {
+            clusterManagementThread.shutdown();
+            try {
+                clusterManagementThread.join();
+            } catch (InterruptedException ignoreInterruptionOfThisThread) {
+            }
         }
         
         try {
@@ -3798,7 +3806,7 @@ public abstract class JobStoreSupport implements JobStore, Constants {
 
     class ClusterManager extends Thread {
 
-        private boolean shutdown = false;
+        private volatile boolean shutdown = false;
 
         private int numFails = 0;
         
@@ -3874,7 +3882,7 @@ public abstract class JobStoreSupport implements JobStore, Constants {
 
     class MisfireHandler extends Thread {
 
-        private boolean shutdown = false;
+        private volatile boolean shutdown = false;
 
         private int numFails = 0;
         
