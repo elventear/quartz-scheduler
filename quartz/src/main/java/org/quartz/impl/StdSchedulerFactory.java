41995c861f9fe8ea68dde8f37582ba52e98df11d
lorban <lorban@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-05-12 08:21:00 +0000

    forgot to call threadExecutor.initialize()

--- quartz/src/main/java/org/quartz/impl/StdSchedulerFactory.java
+++ quartz/src/main/java/org/quartz/impl/StdSchedulerFactory.java
@@ -1226,6 +1226,8 @@ public class StdSchedulerFactory implements SchedulerFactory {
             SchedulerDetailsSetter.setDetails(tp, schedName, schedInstId);
 
             rsrcs.setThreadExecutor(threadExecutor);
+            threadExecutor.initialize();
+
             rsrcs.setThreadPool(tp);
             if(tp instanceof SimpleThreadPool) {
                 ((SimpleThreadPool)tp).setThreadNamePrefix(schedName + "_Worker");
