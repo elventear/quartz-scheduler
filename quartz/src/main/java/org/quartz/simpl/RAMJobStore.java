2c7e888ef92a99fc577fbb746bc6357093bc03e2
light5 <light5@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-08-24 03:57:31 +0000

    Fixed QTZ-202 on RAMJobStore. Added new DisallowConcurrentExecutionJobTest for the fix as well.

--- quartz/src/main/java/org/quartz/simpl/RAMJobStore.java
+++ quartz/src/main/java/org/quartz/simpl/RAMJobStore.java
@@ -1385,16 +1385,23 @@ public class RAMJobStore implements JobStore {
     public List<OperableTrigger> acquireNextTriggers(long noLaterThan, int maxCount, long timeWindow) {
         synchronized (lock) {
             List<OperableTrigger> result = new ArrayList<OperableTrigger>();
-
+            Set<JobKey> acquiredJobKeysForNoConcurrentExec = new HashSet<JobKey>();
+            Set<TriggerWrapper> excludedTriggers = new HashSet<TriggerWrapper>();
+            
+            // return empty list if store has no triggers.
+            if (timeTriggers.size() == 0)
+            	return result;
+            
             while (true) {
                 TriggerWrapper tw;
 
                 try {
                     tw = (TriggerWrapper) timeTriggers.first();
-                    if (tw == null) return result;
+                    if (tw == null)
+                    	break;
                     timeTriggers.remove(tw);
                 } catch (java.util.NoSuchElementException nsee) {
-                    return result;
+                    break;
                 }
 
                 if (tw.trigger.getNextFireTime() == null) {
@@ -1410,18 +1417,35 @@ public class RAMJobStore implements JobStore {
 
                 if (tw.getTrigger().getNextFireTime().getTime() > noLaterThan + timeWindow) {
                     timeTriggers.add(tw);
-                    return result;
+                    break;
                 }
+                
+            	// If trigger's job is set as @DisallowConcurrentExecution, and it has already been added to result, then
+                // put it back into the timeTriggers set and continue to search for next trigger.
+            	JobKey jobKey = tw.trigger.getJobKey();
+            	JobDetail job = jobsByKey.get(tw.trigger.getJobKey()).jobDetail;
+            	if (job.isConcurrentExectionDisallowed()) {
+            		if (acquiredJobKeysForNoConcurrentExec.contains(jobKey)) {
+	            		excludedTriggers.add(tw);
+	            		continue; // go to next trigger in store.
+            		} else {
+                        acquiredJobKeysForNoConcurrentExec.add(jobKey);
+            		}
+            	}
 
                 tw.state = TriggerWrapper.STATE_ACQUIRED;
-
                 tw.trigger.setFireInstanceId(getFiredTriggerRecordId());
                 OperableTrigger trig = (OperableTrigger) tw.trigger.clone();
                 result.add(trig);
 
                 if (result.size() == maxCount)
-                  return result;
+                	break;
             }
+            
+            // If we did excluded triggers to prevent ACQUIRE state due to DisallowConcurrentExecution, we need to add them back to store.
+            if (excludedTriggers.size() > 0)
+            	timeTriggers.addAll(excludedTriggers);
+            return result;
         }
     }
 
