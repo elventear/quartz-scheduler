ab0a683fad4dadcf5ec02a49df87d81bba09136b
light5 <light5@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-08-26 19:03:24 +0000

    Removed println in DailyTimeIntervalTriggerImplTest

--- quartz/src/test/java/org/quartz/impl/triggers/DailyTimeIntervalTriggerImplTest.java
+++ quartz/src/test/java/org/quartz/impl/triggers/DailyTimeIntervalTriggerImplTest.java
@@ -55,7 +55,6 @@ public class DailyTimeIntervalTriggerImplTest extends TestCase {
 		trigger.setRepeatInterval(72);
 		
 		List<Date> fireTimes = TriggerUtils.computeFireTimes(trigger, null, 48);
-		int i=0; for (Date t : fireTimes) System.out.println("#"+(i++)+ " " + t);
 		Assert.assertEquals(48, fireTimes.size());
 		Assert.assertEquals(dateOf(8, 0, 0, 1, 1, 2011), fireTimes.get(0));
 		Assert.assertEquals(dateOf(10, 24, 0, 16, 1, 2011), fireTimes.get(47));
