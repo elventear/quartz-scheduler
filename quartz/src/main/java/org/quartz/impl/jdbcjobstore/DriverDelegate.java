b8333c4e4f26a0e15aa9e490331aff36cbdbeb68
light5 <light5@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-08-27 13:37:54 +0000

    Re-added old DriverDelegate#selectTriggerToAcquire method for compatibility reason. It is marked as deprecated.

--- quartz/src/main/java/org/quartz/impl/jdbcjobstore/DriverDelegate.java
+++ quartz/src/main/java/org/quartz/impl/jdbcjobstore/DriverDelegate.java
@@ -924,6 +924,26 @@ public interface DriverDelegate {
      *          highest value of <code>getNextFireTime()</code> of the triggers (exclusive)
      * @param noEarlierThan 
      *          highest value of <code>getNextFireTime()</code> of the triggers (inclusive)
+     *          
+     * @return A (never null, possibly empty) list of the identifiers (Key objects) of the next triggers to be fired.
+     * 
+     * @deprecated - This remained for compatibility reason. Use {@link #selectTriggerToAcquire(Connection, long, long, int)} instead. 
+     */
+    public List<TriggerKey> selectTriggerToAcquire(Connection conn, long noLaterThan, long noEarlierThan)
+        throws SQLException;
+    
+    /**
+     * <p>
+     * Select the next trigger which will fire to fire between the two given timestamps 
+     * in ascending order of fire time, and then descending by priority.
+     * </p>
+     * 
+     * @param conn
+     *          the DB Connection
+     * @param noLaterThan
+     *          highest value of <code>getNextFireTime()</code> of the triggers (exclusive)
+     * @param noEarlierThan 
+     *          highest value of <code>getNextFireTime()</code> of the triggers (inclusive)
      * @param maxCount 
      *          maximum number of trigger keys allow to acquired in the returning list.
      *          
--- quartz/src/main/java/org/quartz/impl/jdbcjobstore/StdJDBCDelegate.java
+++ quartz/src/main/java/org/quartz/impl/jdbcjobstore/StdJDBCDelegate.java
@@ -2510,6 +2510,29 @@ public class StdJDBCDelegate implements DriverDelegate, StdJDBCConstants {
             closeStatement(ps);
         }
     }
+    
+    /**
+     * <p>
+     * Select the next trigger which will fire to fire between the two given timestamps 
+     * in ascending order of fire time, and then descending by priority.
+     * </p>
+     * 
+     * @param conn
+     *          the DB Connection
+     * @param noLaterThan
+     *          highest value of <code>getNextFireTime()</code> of the triggers (exclusive)
+     * @param noEarlierThan 
+     *          highest value of <code>getNextFireTime()</code> of the triggers (inclusive)
+     *          
+     * @return A (never null, possibly empty) list of the identifiers (Key objects) of the next triggers to be fired.
+     * 
+     * @deprecated - This remained for compatibility reason. Use {@link #selectTriggerToAcquire(Connection, long, long, int)} instead. 
+     */
+    public List<TriggerKey> selectTriggerToAcquire(Connection conn, long noLaterThan, long noEarlierThan)
+            throws SQLException {
+    	// This old API used to always return 1 trigger.
+    	return selectTriggerToAcquire(conn, noLaterThan, noEarlierThan, 1);
+	}
 
     /**
      * <p>
