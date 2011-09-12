7a045363f1f75ad485bf63d7538f6502cb603371
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-09-12 14:17:22 +0000

    another attempt to remove race.

--- quartz/src/test/java/org/quartz/Qtz205SchedulerListenerTest.java
+++ quartz/src/test/java/org/quartz/Qtz205SchedulerListenerTest.java
@@ -152,12 +152,14 @@ public class Qtz205SchedulerListenerTest  {
 		scheduler.getListenerManager().addSchedulerListener(schedulerListener);
 		scheduler.getListenerManager().addTriggerListener(triggerListener);
 		scheduler.start();
+		scheduler.standby();
 		
 		JobDetail job = newJob(Qtz205Job.class).withIdentity("test").build();
 		Trigger trigger = newTrigger().withIdentity("test")
 				.withSchedule(simpleSchedule().withIntervalInMilliseconds(250).withRepeatCount(2))
 				.build();
 		scheduler.scheduleJob(job, trigger);
+		scheduler.start();
 		Thread.sleep(3000);
 		
 		scheduler.shutdown(true);
