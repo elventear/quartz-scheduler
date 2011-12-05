0d10af31a33d165ef72a39f8730bc0139b3a1856
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-12-05 22:26:54 +0000

    merge 1775,1776,1777

--- quartz/src/main/java/org/quartz/CronScheduleBuilder.java
+++ quartz/src/main/java/org/quartz/CronScheduleBuilder.java
@@ -158,6 +158,40 @@ public class CronScheduleBuilder extends ScheduleBuilder<CronTrigger> {
 
         return cronScheduleNoParseException(cronExpression);
     }
+    
+    /**
+     * Create a CronScheduleBuilder with a cron-expression that sets the
+     * schedule to fire at the given day at the given time 
+     * (hour and minute) on the given days of the week.
+     * 
+     * @param daysOfWeek the dasy of the week to fire
+     * @param hour the hour of day to fire
+     * @param minute the minute of the given hour to fire
+     * @return the new CronScheduleBuilder
+     * @see CronExpression
+     * @see DateBuilder#MONDAY
+     * @see DateBuilder#TUESDAY
+     * @see DateBuilder#WEDNESDAY
+     * @see DateBuilder#THURSDAY
+     * @see DateBuilder#FRIDAY
+     * @see DateBuilder#SATURDAY
+     * @see DateBuilder#SUNDAY
+     */
+    public static CronScheduleBuilder atHourAndMinuteOnGivenDaysOfWeek(int hour, int minute, Integer ... daysOfWeek)  {
+    	if(daysOfWeek == null || daysOfWeek.length == 0)
+    		throw new IllegalArgumentException("You must specify at least one day of week.");
+    	for(int dayOfWeek : daysOfWeek)
+    		DateBuilder.validateDayOfWeek(dayOfWeek);
+        DateBuilder.validateHour(hour);
+        DateBuilder.validateMinute(minute);
+
+        String cronExpression = String.format("0 %d %d ? * %d", minute, hour, daysOfWeek[0]);
+        
+        for(int i=1; i < daysOfWeek.length; i++)
+        	cronExpression = cronExpression + "," + daysOfWeek[i];
+
+        return cronScheduleNoParseException(cronExpression);
+    }
 
     /**
      * Create a CronScheduleBuilder with a cron-expression that sets the
