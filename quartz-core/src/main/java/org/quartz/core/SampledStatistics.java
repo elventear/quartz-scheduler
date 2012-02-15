74a8204674b0c1a73ac96110bb488a58cf769379
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-02-15 02:01:36 +0000

    removed to resync

--- quartz-core/src/main/java/org/quartz/core/SampledStatistics.java
+++
@@ -1,8 +0,0 @@
-package org.quartz.core;
-
-public interface SampledStatistics {
-    long getJobsScheduledMostRecentSample();
-    long getJobsExecutingMostRecentSample();
-    long getJobsCompletedMostRecentSample();
-    void shutdown();
-}
