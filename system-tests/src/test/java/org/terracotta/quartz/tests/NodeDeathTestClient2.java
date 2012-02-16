82d05e23c1207058829f39f496ccdb98c927e253
teck <teck@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-02-16 00:13:18 +0000

    don't ever timeout waiting for first client

--- system-tests/src/test/java/org/terracotta/quartz/tests/NodeDeathTestClient2.java
+++ system-tests/src/test/java/org/terracotta/quartz/tests/NodeDeathTestClient2.java
@@ -20,15 +20,17 @@ public class NodeDeathTestClient2 extends ClientBase {
   @Override
   protected void test(Scheduler scheduler) throws Throwable {
     Socket s = null;
-    for (int i = 0; i < 30; i++) {
+
+    // make this loop forever in case a hang in the first client prevents the listener from ever starting. This loop
+    // should make the test timeout and thus we'll get a thread dump
+    while (true) {
       try {
         s = new Socket("127.0.0.1", node1Port);
+        break;
       } catch (ConnectException ce) {
         Thread.sleep(1000L);
       }
     }
-
-    if (s == null) { throw new AssertionError("no listening port found on " + node1Port); }
 
     InputStream in = s.getInputStream();
 
@@ -45,7 +47,6 @@ public class NodeDeathTestClient2 extends ClientBase {
         break;
       }
     }
-
   }
 
   private void log(String msg) {
