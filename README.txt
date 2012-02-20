decb917817a0695be5a1d6ac5532eccfd025a130
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-02-20 21:43:20 +0000

    use stubs to get around missing oracle, weblogic maven artifacts

--- README.txt
+++ README.txt
@@ -3,28 +3,7 @@
   
 Note:  the final Quartz jar is found under quartz/target 
 
-2. Compiling quartz-oracle and quartz-weblogic:
-
-    If you don't have Oracle and/or Weblogic third party libraries jars required for compilation (these can't be found
-    on public Maven repos due to licensing) you have 2 options:
-    
-  a. run Maven with -Dno-oracle-weblogic : this will exclude those 2 modules
-  
-  b. download and install needed artifacts
-     
-  For Oracle: ojdbc5.jar can be found at http://www.oracle.com/technetwork/database/enterprise-edition/jdbc-112010-090769.html
-  For Weblogic: com.bea.core.datasource_1.6.0.0.jar can be found under "modules" of a Weblogic installation
-  
-  Once you have the jars, install them to Maven local repo:
-  
-  %> mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc5 \
-         -Dversion=11.2.0.1.0 -Dpackaging=jar -Dfile=ojdbc5.jar
-         
-  %> mvn install:install-file -DgroupId=com.bea.core -DartifactId=datasource \
-         -Dversion=1.6.0 -Dpackaging=jar -Dfile=com.bea.core.datasource_1.6.0.0.jar
-         
-         
-3. To build Quartz distribution kit:
+2. To build Quartz distribution kit:
 
   %> cd distribution
   %> mvn package
--- quartz-weblogic/src/main/java/org/quartz/impl/jdbcjobstore/oracle/weblogic/WebLogicOracleDelegate.java
+++ quartz-weblogic/src/main/java/org/quartz/impl/jdbcjobstore/oracle/weblogic/WebLogicOracleDelegate.java
@@ -14,12 +14,14 @@
  * under the License.
  * 
  */
- 
+
 package org.quartz.impl.jdbcjobstore.oracle.weblogic;
 
-import org.slf4j.Logger;
 import org.quartz.impl.jdbcjobstore.oracle.OracleDelegate;
 import org.quartz.spi.ClassLoadHelper;
+import org.slf4j.Logger;
+
+import weblogic.jdbc.vendor.oracle.OracleThinBlob;
 
 import java.lang.reflect.Method;
 import java.sql.Blob;
@@ -27,81 +29,74 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Handle Blobs correctly when Oracle is being used inside of Weblogic 8.1,
- * as discussed at: http://edocs.bea.com/wls/docs81/jdbc/thirdparty.html#1043705
- *  
+ * Handle Blobs correctly when Oracle is being used inside of Weblogic 8.1, as discussed at:
+ * http://edocs.bea.com/wls/docs81/jdbc/thirdparty.html#1043705
+ * 
  * @see org.quartz.impl.jdbcjobstore.WebLogicDelegate
  * @author James House
  * @author Igor Fedulov <a href="mailto:igor@fedulov.com">igor@fedulov.com</a>
  */
 public class WebLogicOracleDelegate extends OracleDelegate {
 
-    /**
-     * <p>
-     * Create new WebLogicOracleDelegate instance.
-     * </p>
-     * 
-     * @param logger
-     *            the logger to use during execution
-     * @param tablePrefix
-     *            the prefix of all table names
-     */
-    public WebLogicOracleDelegate(Logger logger, String tablePrefix,
-            String schedName, String instanceId, ClassLoadHelper classLoadHelper) {
-        super(logger, tablePrefix, schedName, instanceId, classLoadHelper);
-    }
+  /**
+   * <p>
+   * Create new WebLogicOracleDelegate instance.
+   * </p>
+   * 
+   * @param logger the logger to use during execution
+   * @param tablePrefix the prefix of all table names
+   */
+  public WebLogicOracleDelegate(Logger logger, String tablePrefix, String schedName, String instanceId,
+                                ClassLoadHelper classLoadHelper) {
+    super(logger, tablePrefix, schedName, instanceId, classLoadHelper);
+  }
 
-    /**
-     * <p>
-     * Create new WebLogicOracleDelegate instance.
-     * </p>
-     * 
-     * @param logger
-     *            the logger to use during execution
-     * @param tablePrefix
-     *            the prefix of all table names
-     * @param useProperties
-     *            use java.util.Properties for storage
-     */
-    public WebLogicOracleDelegate(Logger logger, String tablePrefix,
-            String schedName, String instanceId, ClassLoadHelper classLoadHelper, Boolean useProperties) {
-        super(logger, tablePrefix, schedName, instanceId, classLoadHelper, useProperties);
-    }
+  /**
+   * <p>
+   * Create new WebLogicOracleDelegate instance.
+   * </p>
+   * 
+   * @param logger the logger to use during execution
+   * @param tablePrefix the prefix of all table names
+   * @param useProperties use java.util.Properties for storage
+   */
+  public WebLogicOracleDelegate(Logger logger, String tablePrefix, String schedName, String instanceId,
+                                ClassLoadHelper classLoadHelper, Boolean useProperties) {
+    super(logger, tablePrefix, schedName, instanceId, classLoadHelper, useProperties);
+  }
 
-    /**
-     * Check for the Weblogic Blob wrapper, and handle accordingly...
-     */
-    @Override
-    protected Blob writeDataToBlob(ResultSet rs, int column, byte[] data) throws SQLException {
-        Blob blob = rs.getBlob(column);
-        
-        if (blob == null) { 
-            throw new SQLException("Driver's Blob representation is null!");
+  /**
+   * Check for the Weblogic Blob wrapper, and handle accordingly...
+   */
+  @Override
+  protected Blob writeDataToBlob(ResultSet rs, int column, byte[] data) throws SQLException {
+    Blob blob = rs.getBlob(column);
+
+    if (blob == null) { throw new SQLException("Driver's Blob representation is null!"); }
+
+    // handle thin driver's blob
+    if (blob instanceof OracleThinBlob) {
+      ((OracleThinBlob) blob).putBytes(1, data);
+      return blob;
+    } else if (blob.getClass().getPackage().getName().startsWith("weblogic.")) {
+      // (more slowly) handle blob for wrappers of other variations of drivers...
+      try {
+        // try to find putBytes method...
+        Method m = blob.getClass().getMethod("putBytes", new Class[] { long.class, byte[].class });
+        m.invoke(blob, new Object[] { new Long(1), data });
+      } catch (Exception e) {
+        try {
+          // Added this logic to the original code from OpenSymphony
+          // putBytes method does not exist. Try setBytes
+          Method m = blob.getClass().getMethod("setBytes", new Class[] { long.class, byte[].class });
+          m.invoke(blob, new Object[] { new Long(1), data });
+        } catch (Exception e2) {
+          throw new SQLException("Unable to find putBytes(long,byte[]) or setBytes(long,byte[]) methods on blob: " + e2);
         }
-        
-        // handle thin driver's blob
-        if (blob instanceof weblogic.jdbc.vendor.oracle.OracleThinBlob) { 
-            ((weblogic.jdbc.vendor.oracle.OracleThinBlob) blob).putBytes(1, data);
-            return blob;
-        } else if(blob.getClass().getPackage().getName().startsWith("weblogic.")) {
-            // (more slowly) handle blob for wrappers of other variations of drivers...
-            try {
-                // try to find putBytes method...
-                Method m = blob.getClass().getMethod("putBytes", new Class[] {long.class, byte[].class});
-                m.invoke(blob, new Object[] {new Long(1), data});
-            } catch (Exception e) {
-                try {
-                    // Added this logic to the original code from OpenSymphony
-                    // putBytes method does not exist. Try setBytes
-                    Method m = blob.getClass().getMethod("setBytes", new Class[] { long.class, byte[].class });
-                    m.invoke(blob, new Object[] { new Long(1), data });
-                } catch (Exception e2) {
-                    throw new SQLException("Unable to find putBytes(long,byte[]) or setBytes(long,byte[]) methods on blob: " + e2);
-                }
-            }
-            return blob;
-        } else {
-            return super.writeDataToBlob(rs, column, data);
-        }
+      }
+      return blob;
+    } else {
+      return super.writeDataToBlob(rs, column, data);
     }
+  }
 }
