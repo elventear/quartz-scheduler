e74233616bf93e9510bc2b3c7cc30f5351dd5eaa
alexsnaps <alexsnaps@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-02-29 20:05:46 +0000

    MNK-3458 Changed test to not loose the thread dump of Client1 when deadlocked (à la NodeDeathTestClient2)

--- system-tests/src/test/java/org/terracotta/quartz/tests/LiveNodeClient2.java
+++ system-tests/src/test/java/org/terracotta/quartz/tests/LiveNodeClient2.java
@@ -51,16 +51,18 @@ public class LiveNodeClient2 extends ClientBase {
     // not strictly necessary but this should make the newly added trigger get ACQUIRED locally
     Thread.sleep(5000L);
 
-    Socket s = null;
-    for (int i = 0; i < 30; i++) {
+    Socket s;
+
+    // make this loop forever in case a hang in the first client prevents the listener from ever starting. This loop
+    // should make the test timeout and thus we'll get a thread dump
+    while (true) {
       try {
         s = new Socket("127.0.0.1", node1Port);
+        break;
       } catch (ConnectException ce) {
         Thread.sleep(1000L);
       }
     }
-
-    if (s == null) { throw new AssertionError("no listening port found on " + node1Port); }
 
     InputStream in = s.getInputStream();
 
