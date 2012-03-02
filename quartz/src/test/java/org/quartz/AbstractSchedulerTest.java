05dfcb33c0269550942a0f75f804fa5874acc15d
jhouse <jhouse@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-03-02 16:35:55 +0000

    better debugging info for MNK-3494

--- quartz/src/test/java/org/quartz/AbstractSchedulerTest.java
+++ quartz/src/test/java/org/quartz/AbstractSchedulerTest.java
@@ -23,6 +23,7 @@ import static org.quartz.TriggerKey.triggerKey;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -262,21 +263,33 @@ public abstract class AbstractSchedulerTest extends TestCase {
       
       Thread.sleep(500L);
       
+      Map<Thread, StackTraceElement[]> allThreadsRunning = Thread.getAllStackTraces();
+
       scheduler.shutdown( true );
       
       Thread.sleep(200L);
 
       Map<Thread, StackTraceElement[]> allThreadsEnd = Thread.getAllStackTraces();
-
+      Set<Thread> endingThreads = new HashSet<Thread>(allThreadsEnd.keySet());
+      // remove all pre-existing threads from the set
       for(Thread t: allThreadsStart.keySet()) {
         allThreadsEnd.remove(t);
       }
-      for(Thread t: allThreadsEnd.keySet()) {
+      // remove threads that are known artifacts of the test
+      for(Thread t: endingThreads) {
         if(t.getName().contains("derby") && t.getThreadGroup().getName().contains("derby")) {
           allThreadsEnd.remove(t);
-          continue;
         }
-        System.out.println("*** Found additional thread: " + t.getName() + " (of type " + t.getClass().getName() +")  in group: " + t.getThreadGroup().getName() + " with parent group: " + (t.getThreadGroup().getParent() == null ? "-none-" : t.getThreadGroup().getParent().getName()));
+      }
+      if(allThreadsEnd.size() > 0) {
+        // log the additional threads
+        for(Thread t: allThreadsEnd.keySet()) {
+          System.out.println("*** Found additional thread: " + t.getName() + " (of type " + t.getClass().getName() +")  in group: " + t.getThreadGroup().getName() + " with parent group: " + (t.getThreadGroup().getParent() == null ? "-none-" : t.getThreadGroup().getParent().getName()));
+        }          
+        // log all threads that were running before shutdown
+        for(Thread t: allThreadsRunning.keySet()) {
+          System.out.println("- Test runtime thread: " + t.getName() + " (of type " + t.getClass().getName() +")  in group: " + t.getThreadGroup().getName() + " with parent group: " + (t.getThreadGroup().getParent() == null ? "-none-" : t.getThreadGroup().getParent().getName()));
+        }          
       }
       assertTrue( "Found unexpected new threads (see console output for listing)", allThreadsEnd.size() == 0  );
     }
