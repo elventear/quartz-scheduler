2943f9066742fe5d497910eb252c8aa40bd4715e
hhuynh <hhuynh@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-04-26 22:17:56 +0000

    fixed stub class OracleDelegate API, types different leading to runtime method not found

--- quartz-stubs/src/main/java/weblogic/jdbc/jts/Driver.java
+++ quartz-stubs/src/main/java/weblogic/jdbc/jts/Driver.java
@@ -24,32 +24,26 @@ import java.util.Properties;
 
 public class Driver implements java.sql.Driver {
 
-  @Override
   public Connection connect(String url, Properties info) throws SQLException {
     return null;
   }
 
-  @Override
   public boolean acceptsURL(String url) throws SQLException {
     return false;
   }
 
-  @Override
   public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
     return null;
   }
 
-  @Override
   public int getMajorVersion() {
     return 0;
   }
 
-  @Override
   public int getMinorVersion() {
     return 0;
   }
 
-  @Override
   public boolean jdbcCompliant() {
     return false;
   }
