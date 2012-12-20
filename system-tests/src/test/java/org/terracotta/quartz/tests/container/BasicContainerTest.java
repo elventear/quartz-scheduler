c059826724a55618c304972e5efe0158bb004371
teck <teck@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2012-12-20 21:38:47 +0000

    fixes for config changes

--- system-tests/src/test/java/org/terracotta/quartz/tests/container/BasicContainerTest.java
+++ system-tests/src/test/java/org/terracotta/quartz/tests/container/BasicContainerTest.java
@@ -1,18 +1,14 @@
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
- * 
  */
 package org.terracotta.quartz.tests.container;
 
@@ -82,7 +78,7 @@ public class BasicContainerTest extends AbstractOneServerDeploymentTest {
     private File createConfigFile() {
       try {
         File configFile = writeConfigFile(TempDirectoryUtil.getTempDirectory(this.getClass()), getServerManager()
-            .getServerTcConfig().getDsoPort());
+            .getServerTcConfig().getTsaPort());
         System.out.println("Wrote temp config file at: " + configFile.getAbsolutePath());
         return configFile;
       } catch (IOException e) {
