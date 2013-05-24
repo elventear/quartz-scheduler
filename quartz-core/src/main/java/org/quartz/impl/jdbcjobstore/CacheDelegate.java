09b91840c0aedaf165b0a132ddccabeb74b40e38
cdennis <cdennis@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2013-05-24 14:28:03 +0000

    QTZ-306 : Modify CacheDelegate to correctly free Blob instances

--- quartz-core/src/main/java/org/quartz/impl/jdbcjobstore/CacheDelegate.java
+++ quartz-core/src/main/java/org/quartz/impl/jdbcjobstore/CacheDelegate.java
@@ -17,12 +17,15 @@
 
 package org.quartz.impl.jdbcjobstore;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.sql.Blob;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import org.quartz.spi.ClassLoadHelper;
-import org.slf4j.Logger;
 
 /**
  * <p>
@@ -54,5 +57,123 @@ public class CacheDelegate extends StdJDBCDelegate {
     protected void setBytes(PreparedStatement ps, int index, ByteArrayOutputStream baos) throws SQLException {
         ps.setObject(index, ((baos == null) ? null : baos.toByteArray()), java.sql.Types.BLOB);
     } 
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Caché requires {@code java.sql.Blob} instances to be explicitly freed.
+     */
+    @Override
+    protected Object getObjectFromBlob(ResultSet rs, String colName) throws ClassNotFoundException, IOException, SQLException {
+        Blob blob = rs.getBlob(colName);
+        if (blob == null) {
+            return null;
+        } else {
+            try {
+                if (blob.length() == 0) {
+                    return null;
+                } else {
+                    InputStream binaryInput = blob.getBinaryStream();
+                    if (binaryInput == null) {
+                        return null;
+                    } else if (binaryInput instanceof ByteArrayInputStream && ((ByteArrayInputStream) binaryInput).available() == 0 ) {
+                        return null;
+                    } else {
+                        ObjectInputStream in = new ObjectInputStream(binaryInput);
+                        try {
+                            return in.readObject();
+                        } finally {
+                            in.close();
+                        }
+                    }
+                }
+            } finally {
+                blob.free();
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Caché requires {@code java.sql.Blob} instances to be explicitly freed.
+     */
+    @Override
+    protected Object getJobDataFromBlob(ResultSet rs, String colName) throws ClassNotFoundException, IOException, SQLException {
+        if (canUseProperties()) {
+            Blob blob = rs.getBlob(colName);
+            if (blob == null) {
+                return null;
+            } else {
+                return new BlobFreeingStream(blob, blob.getBinaryStream());
+            }
+        } else {
+            return getObjectFromBlob(rs, colName);
+        }
+    }
+    
+    private static class BlobFreeingStream extends InputStream {
+        
+        private final Blob source;
+        private final InputStream delegate;
+
+        private BlobFreeingStream(Blob blob, InputStream stream) {
+            this.source = blob;
+            this.delegate = stream;
+        }
+
+        @Override
+        public int read() throws IOException {
+            return delegate.read();
+        }
+
+        @Override
+        public int read(byte[] b) throws IOException {
+            return delegate.read(b);
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            return delegate.read(b, off, len);
+        }
+
+        @Override
+        public long skip(long n) throws IOException {
+            return delegate.skip(n);
+        }
+
+        @Override
+        public int available() throws IOException {
+            return delegate.available();
+        }
+
+        @Override
+        public void close() throws IOException {
+            try {
+                delegate.close();
+            } finally {
+                try {
+                    source.free();
+                } catch (SQLException ex) {
+                    throw new IOException(ex);
+                }
+            }
+        }
+
+        @Override
+        public synchronized void mark(int readlimit) {
+            delegate.mark(readlimit);
+        }
+
+        @Override
+        public synchronized void reset() throws IOException {
+            delegate.reset();
+        }
+
+        @Override
+        public boolean markSupported() {
+            return delegate.markSupported();
+        }
+    }
 }
 
